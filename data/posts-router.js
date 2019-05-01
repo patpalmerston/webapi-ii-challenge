4d1c1627c12caa6f2e8145ddd73bf6d3fc0b0bef
patpalmerston <patpalmerston@gmail.com>
2019-04-30 17:13:10 -0700

    mvp2

--- data/posts-router.js
+++ data/posts-router.js
@@ -111,28 +111,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-  
- 
-
-
-// router.post("/", async (req, res) => {
-//   const post = req.body;
-//   if (post.title && post.contents) {
-//     db.insert(post)
-//       .then(post => {
-//         res.status(200).json(post);
-//       })
-//       .catch(err => {
-//         res.status(500).json({
-//           message: "Error retrieving the posts"
-//         });
-//       });
-//   } else {
-//     res.status(400).json({
-//       errorMessage: "There was an error while saving the post to the database."
-//     });
-//   }
-// });
 
 
 module.exports = router;
